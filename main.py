91a84a0e20b8a674c6682551dcd66fc87206ce9a
nastinsk <nastinsk@gmail.com>
2020-02-24 11:28:40 -0800

    libraries added for ML

---
+++ main.py
@@ -0,0 +1,7 @@
+import os
+import librosa #for audio processing
+import IPython.display as ipd
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.io import wavfile #for audio processing
+
